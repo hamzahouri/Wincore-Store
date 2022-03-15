454c45421d3295c4b4569184030bfff9d338358b
unknown <hamzahouri@gmail.com>
2022-03-15 15:08:42 +0100

    ading stripe api for payement processing

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems,selectCartTotal} from '../../redux/user/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
-
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component.jsx'
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -30,6 +30,12 @@ const CheckoutPage = ({ cartItems, total }) => (
        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className='total'>TOTAL: ${total}</div>
+      <div className='test-warning'>
+  Please use the following test credit card for payement
+  <br/>
+  4242 4242 4242 4242 -EXP : 01/20 - CVV : 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 
